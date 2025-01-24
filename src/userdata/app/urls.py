2e88de468d0132185f4be5d4fe38a53815a3bb8e
Thomas Ho <thofting@student.42berlin.de>
2025-01-24 17:20:27 +0100

    init for oauth work

--- src/userdata/app/urls.py
+++ src/userdata/app/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('user-api/admin/', admin.site.urls),
     path('user-api/', include('user_management.urls')),
     path('user-api/', include('game_data.urls'))
 ]
